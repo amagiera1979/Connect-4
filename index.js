80d76392fbc4db2f3a9f90da4e41c6692531b295
Agnieszka Magiera <agnieszka_magiera@wp.pl>
2024-02-13 19:57:38 +0100

    Podpiecie pliku .js do .html

--- index.js
+++ index.js
@@ -3,31 +3,31 @@ const player2 = document.querySelector('.p-two');
 const movePlayer1 = document.querySelector('.score-p1');
 const movePlayer2 = document.querySelector('.score-p2');
 
-
-
-function createCirkleRed() {
-    const cirkleYellow = document.createElement('div');
-    cirkleYellow.backgroundColor = 'rgb(241, 207, 10);';
-    cirkleYellow.width = '60px';
-    cirkleYellow.height = '60px';
-    cirkleYellow.borderRadius = '50%';
-}
 function createCirkleYellow() {
-    const cirkleRed = document.createElement('div');
-    cirkleRed.style.backgroundColor = 'red';
-    cirkleRed.style.width = '60px';
-    cirkleRed.style.height = '60px';
-    cirkleRed.style.borderRadius = '50%';
+  const cirkleYellow = document.createElement('div');
+  cirkleYellow.backgroundColor = 'rgb(241, 207, 10);';
+  cirkleYellow.width = '60px';
+  cirkleYellow.height = '60px';
+  cirkleYellow.borderRadius = '50%';
+  body.appendChild(cirkleYellow);
+}
+function createCirkleRed() {
+  const cirkleRed = document.createElement('div');
+  cirkleRed.style.backgroundColor = 'red';
+  cirkleRed.style.width = '60px';
+  cirkleRed.style.height = '60px';
+  cirkleRed.style.borderRadius = '50%';
+  body.appendChild(cirkleRed);
 }
 
-// function showPlayer(){
-//    createCirkleRed();
-  
+function showPlayer() {
+  player1.style.backgroundColor = 'rgb(212, 197, 177);';
+  player1.style.border = '2px solid rgb(165, 129, 111);';
+  createCirkleRed();
+  console.log(cirkleRed);
+}
 
-
-// }
-
-window.player1.addEventListener('click', e=>{
-    console.log(e.clientX, e.currentTarget, e.type, e.timeStamp, screenX)
-   })
-// window.addEventListener('load', showPlayer)
+document.addEventListener('click', e => {
+  console.log(e.clientX, e.currentTarget, e.type, e.timeStamp, e.screenX);
+});
+window.addEventListener('load', showPlayer);
