b15819ea0304f0dcba122a09f60d9267d511fcea
Agnieszka Magiera <agnieszka_magiera@wp.pl>
2024-02-14 12:59:41 +0100

    Poprawki w stylowaniu wywolania f.utworzenia dysku

--- index.js
+++ index.js
@@ -4,21 +4,23 @@ const movePlayer1 = document.querySelector('.score-p1');
 const movePlayer2 = document.querySelector('.score-p2');
 const road = document.querySelector('.road');
 
+
 function createCirkleYellow() {
   const cirkleYellow = document.createElement('div');
-  cirkleYellow.className = 'yellow-disc'
+  cirkleYellow.className = 'yellow-disc';
+  player2.style.backgroundColor = 'bisque';
   road.appendChild(cirkleYellow);
 }
 function createCirkleRed() {
   const cirkleRed = document.createElement('div');
   cirkleRed.className = 'red-disc';
-
+  player1.style.backgroundColor = 'bisque';
   road.appendChild(cirkleRed);
 }
 
 function showPlayer() {
-  createCirkleYellow();
-  createCirkleRed();
+  // createCirkleYellow();
+  // createCirkleRed();
 }
 
 document.addEventListener('click', e => {
