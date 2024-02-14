11e6f0574af7a9d038090be8dd3ff9d8e0c0b3c3
Agnieszka Magiera <agnieszka_magiera@wp.pl>
2024-02-14 12:44:52 +0100

    Przeniesienie stylowania dysków z .js do .css

--- index.js
+++ index.js
@@ -6,26 +6,13 @@ const road = document.querySelector('.road');
 
 function createCirkleYellow() {
   const cirkleYellow = document.createElement('div');
-  cirkleYellow.backgroundColor = 'goldenrod';
-  cirkleYellow.width = '60px';
-  cirkleYellow.height = '60px';
-  cirkleYellow.borderRadius = '50%';
-  cirkleYellow.style.left = '759px';
-  cirkleYellow.style.top = '289px';
-  cirkleYellow.style.position = 'absolute';
+  cirkleYellow.className = 'yellow-disc'
   road.appendChild(cirkleYellow);
 }
 function createCirkleRed() {
   const cirkleRed = document.createElement('div');
-  cirkleRed.style.backgroundColor = 'red';
-  cirkleRed.style.width = '60px';
-  cirkleRed.style.height = '60px';
-  cirkleRed.style.borderRadius = '50%';
-  cirkleRed.style.left = '132px';
-  cirkleRed.style.top = '289px';
-  //   cirkleRed.style.display= 'flex';
-  cirkleRed.style.position = 'absolute';
-  //   cirkleRed.style.transform = 'translate(-50%, -50%)'
+  cirkleRed.className = 'red-disc';
+
   road.appendChild(cirkleRed);
 }
 
