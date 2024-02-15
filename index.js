8c76d3548c9744a4e0761a25a0023cf500b85490
Agnieszka Magiera <agnieszka_magiera@wp.pl>
2024-02-15 20:20:35 +0100

    Tworzenie animacji .arrow - niedokonczona

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const player2 = document.querySelector('.p-two');
 const movePlayer1 = document.querySelector('.score-p1');
 const movePlayer2 = document.querySelector('.score-p2');
 const road = document.querySelector('.road');
+const rowDrop = document.querySelectorAll('.row');
 
 function onDragCirkle(e) {
   console.log(e.pageX, e.pageY, 'dragging');
 }
+
+// function dropPlace(e){
+// console.log(e.value, e.target)
+
+// }
 
 function createCirkleYellow() {
   const cirkleYellow = document.createElement('div');
@@ -27,11 +33,31 @@ function createCirkleRed() {
   cirkleRed.addEventListener('dragstart', e => {
     dragged = e.target;
     e.dataTransfer.effectAllowed ='move'
-    console.log(e.target, e.dataTransfer)
+    // console.log(e.target, e.dataTransfer)
     // cirkleRed.style.opacity = '.9';
+    console.log(e.clientX, e.clientY)
   });
+
+  // cirkleRed.addEventListener('dragover', e=>{
+    
+  //   e.preventDefault();
+  // })
+  rowDrop.forEach(r=>{
+    r.addEventListener('dragover', e=>{
+      const arrow = document.createElement('div');
+      arrow.classList = 'arrow'
+      if(cirkleRed.clientX >=184&&cirkleRed.clientY>=239 ) {
+// arrow.style.marginLeft ='20px !importand'
+arrow.style.position = 'flex';
+arrow.style.left = '5%'
+      }
+      road.appendChild(arrow);
+    })
+  })
+
   cirkleRed.addEventListener('dragend', e =>{
     e.dataTransfer.dropEffect ='move'
+    console.log(e.clientX, e.clientY)
   })
 
   // cirkleRed.addEventListener('dragend', e => {
